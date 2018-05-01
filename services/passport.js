446391b97e0231e36eb8c8788d6995d8fad16fca
Robert Powers <bobpowers@me.com>
2018-05-01 16:33:04 -0500

    Refactor passport.js

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,15 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then(existingUser => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then(user => done(null, user));
-				}
-			});
-			// console.log(`Access Token: ${accessToken}`);
-			// console.log(`Refresh Token: ${refreshToken}`);
-			// console.log(`Profile: ` + JSON.stringify(profile, null, 2));
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
