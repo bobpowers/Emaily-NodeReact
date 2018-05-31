3a6048de6198ff1f9a5f53a314538eeb41979ffe
Robert Powers <bobpowers@me.com>
2018-05-31 11:16:30 -0500

    Setup for React Router and add Materialize package

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//dummy components
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-	return <div>WORKING</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Header />
+					{/* using 'exact' as a prop is the same as writing exact={true} */}
+					<Route exact path="/" component={Landing} />
+					<Route exact path="/surveys" component={Dashboard} />
+					<Route path="/surveys/new" component={SurveyNew} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
