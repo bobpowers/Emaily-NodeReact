681da7aaf85f30c979c574806f42539220ca8d4e
Robert Powers <bobpowers@me.com>
2018-06-01 13:08:02 -0500

    Add async await to reducer. Add Auth check to App js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 //dummy components
 import Header from './Header';
@@ -7,20 +9,29 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-	return (
-		<div className="container">
-			<BrowserRouter>
-				<div>
-					<Header />
-					{/* using 'exact' as a prop is the same as writing exact={true} */}
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/surveys" component={Dashboard} />
-					<Route path="/surveys/new" component={SurveyNew} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
+class App extends Component {
+	// using 'DidMount' instead of 'WillMount' because 'Did' is viewed as the preferred action for initial AJAX requests. And the difference in speed/timing between the two has virtually no difference
+	componentDidMount() {
+		//the access to props.fetchUser() is provided by the exported 'connect' function at the bottom. It is housed in 'actions'
+		this.props.fetchUser();
+	}
 
-export default App;
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						{/* using 'exact' as a prop is the same as writing exact={true} */}
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+//the 'actions' are assigned to the App component as props
+export default connect(null, actions)(App);
