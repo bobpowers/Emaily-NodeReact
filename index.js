f26b35c9c6dfe9e48db9bb4b80162ca558937b2b
Robert Powers <bobpowers@me.com>
2018-06-03 13:14:02 -0500

    Add production code to express server to return react build for unrecognized routes

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+//this is production code for serving routes defined in our react app that the express server isnt set up for.
+if (process.env.NODE_ENV === 'production') {
+	//express will serve up production assets like main.js or main.css
+	app.use(express.static('client/build'));
+	//express will serve up index.html file if it doesnt recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log(`Listening on port: ${PORT}`);
