ef354e8891427cc5fa6dc6ba5c89cc77519740e4
Robert Powers <bobpowers@me.com>
2018-06-05 11:08:44 -0500

    Include Survey model to surveyRoutes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
+		//wrap the email (in map function) in parenthesis to return an object so the function wont confuse it as function brackets
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: Date.now()
+		});
+	});
 };
