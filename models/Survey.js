983353265d6726d22bf85d869297e61fb30dba06
Robert Powers <bobpowers@me.com>
2018-06-05 10:32:04 -0500

    Add several new fields to the end of Survey schema

--- models/Survey.js
+++ models/Survey.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const RecipientSchema = require('./Recipient');
 
+//the _user field is not required. the '_' is a convention to convey a relationship. its a reference field.
+
 const surveySchema = new Schema({
 	title: String,
 	body: String,
 	subject: String,
 	recipients: [RecipientSchema],
 	yes: { type: Number, default: 0 },
-	no: { type: Number, default: 0 }
+	no: { type: Number, default: 0 },
+	_user: { type: Schema.Types.ObjectId, ref: 'User' },
+	dateSent: Date,
+	lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
