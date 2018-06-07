118d01ce91d104ce689bd88c1e21b9a3995cd1ab
Robert Powers <bobpowers@me.com>
2018-06-07 11:11:14 -0500

    Add some logic and comments to survey components

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,21 @@
+//Displays form for user to add input
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
 	render() {
-		return <div>SurveyForm</div>;
+		return (
+			<div>
+				{/* handleSubmit is provided by reduxForm helper*/}
+				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+					<Field type="text" name="surveyTitle" component="input" />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
 	}
 }
 
-export default SurveyForm;
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyForm);
