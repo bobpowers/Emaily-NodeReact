f302226edd2dcd214d08b1909daed023efd1ca37
Robert Powers <bobpowers@me.com>
2018-06-07 10:51:34 -0500

    Add survey component folder and import new component to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
 	// using 'DidMount' instead of 'WillMount' because 'Did' is viewed as the preferred action for initial AJAX requests. And the difference in speed/timing between the two has virtually no difference
