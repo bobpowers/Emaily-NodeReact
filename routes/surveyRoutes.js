b2e7a7624b4b382c67849a93be604e18d9952c12
Robert Powers <bobpowers@me.com>
2018-06-07 09:57:38 -0500

    Add survey template edits and some route adjustments

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,9 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thank you for your feedback!');
+	});
 	//the anonymous function is async so that we can push to mongo after the emails have been sent
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
