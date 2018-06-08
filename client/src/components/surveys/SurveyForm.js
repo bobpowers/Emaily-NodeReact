addfef1eb2d16234d9b5ed9fa887b51676ab0a9b
Robert Powers <bobpowers@me.com>
2018-06-08 12:32:32 -0500

    Add form validation for email. Add validate email util

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 import { Link } from 'react-router-dom';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
 	{ label: 'Survey Title', name: 'title' },
@@ -49,6 +50,10 @@ class SurveyForm extends Component {
 function validate(values) {
 	const errors = {};
 
+	//This runs every email through regex and returns bad emails to client
+	errors.emails = validateEmails(values.emails || '');
+
+	//This covers the basics of needing values and it will override the invalid emails warning until the user starts including emails
 	FIELDS.forEach(({ name }) => {
 		if (!values[name]) {
 			errors[name] = 'You must provide a value';
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,17 @@
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
+	const invalidEmails = emails
+		.replace(/(^[,\s]+)|([,\s]+$)/g, '')
+		.split(',')
+		.map(email => email.trim())
+		.filter(email => regex.test(email) === false);
+
+	if (invalidEmails.length) {
+		return `These emails are invalid: ${invalidEmails}`;
+	}
+
+	return;
+};
+
+// .replace(/(^[,\s]+)|([,\s]+$)/g, '')
